ccb96dd1ab99626d9d58d86e36289a0829c3fb40
ClementRoelens <clement.roelens@hotmail.com>
2022-05-03 18:55:59 +0200

    Recherches par genre et par réalisateur fonctionnelles

--- backend/controllers/film.js
+++ backend/controllers/film.js
@@ -1,18 +1,17 @@
-const film = require('../models/film');
 const Film = require('../models/film');
 const fs = require('fs');
 
 exports.tousLesFilms = (req, res, next) => {
     Film.find()
-        .then(films => { 
+        .then(films => {
             var filmsTransmis = [];
             films.forEach(film => {
                 var tempFilm = film.toJSON();
-                tempFilm.date = tempFilm.date.toLocaleDateString();  
-                filmsTransmis.push(tempFilm);  
+                tempFilm.date = tempFilm.date.toLocaleDateString();
+                filmsTransmis.push(tempFilm);
             });
-            
-            res.status(200).json(filmsTransmis) 
+
+            res.status(200).json(filmsTransmis)
         })
         .catch((error) => {
             console.log("erreur : " + error);
@@ -20,14 +19,76 @@ exports.tousLesFilms = (req, res, next) => {
         })
 };
 
+exports.filmsParGenre = (req, res, next) => {
+    // On va récupérer tous les films appartenant au moins à un des genres passés en URL
+    var genreAchercher = req.params.genre;
+    Film.find()
+        .then(films => {
+            var filmsTransmis = [];
+            films.forEach(filmAtrier => {
+                var genresAtrier = filmAtrier.genre;
+                // Le test est true si au moins un des genres du film itéré est le genre cherché
+                if (genresAtrier.includes(genreAchercher)){
+                    // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
+                    var tempFilm = filmAtrier.toJSON();
+                    tempFilm.date = tempFilm.date.toLocaleDateString();
+                    filmsTransmis.push(tempFilm);
+                }
+            });
+            res.status(200).json(filmsTransmis);
+        })
+        .catch(error => res.status(404).json(error))
+    
+};
+
+// Si on veut rechercher plusieurs genres 
+// exports.filmsParGenre = (req, res, next) => {
+//     // On va récupérer tous les films appartenant au moins à un des genres passés en URL
+//     var genresDeTri = req.query.genre;
+//     Film.find()
+//         .then(films => {
+//             var filmsTransmis = [];
+//             films.forEach(filmAtrier => {
+//                 var genresAtrier = filmAtrier.genre;
+//                 // Le test est true si au moins un des genres du film itéré est contenu dans l'array des genres cherchés
+//                 var test = genresAtrier.some(genre => genresDeTri.includes(genre));
+//                 // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
+//                 if (test){
+//                     filmsTransmis.push(filmAtrier);
+//                 }
+//             });
+//             res.status(200).json(filmsTransmis);
+//         })
+//         .catch()
+    
+// };
+
+exports.filmsParReal = (req,res,next) => {
+    var real = req.params.real;
+    Film.find()
+    .then(films=>{
+        var filmsTransmis = [];
+        films.forEach(filmAtrier => {
+            if (filmAtrier.realisateur === real){
+                  // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
+                  var tempFilm = filmAtrier.toJSON();
+                  tempFilm.date = tempFilm.date.toLocaleDateString();
+                  filmsTransmis.push(tempFilm);
+            }
+        });
+        res.status(200).json(filmsTransmis);
+    })
+    .catch(error => res.status(404).json(error))
+};
+
 exports.unFilm = (req, res, next) => {
     Film.findOne({
         _id: req.params.id
     }).then(filmBrut => {
         // Le format de date n'étant pas désiré, on convertit l'objet en JSON pour pouvoir convertir la propriété au format voulu
-        var film = filmBrut.toJSON();
-        film.date = film.date.toLocaleDateString();
-        res.status(200).json(film);
+        var filmTransmis = filmBrut.toJSON();
+        filmTransmis.date = filmTransmis.date.toLocaleDateString();
+        res.status(200).json(filmTransmis);
     })
         .catch(error => res.status(404).json(error))
 };
@@ -36,27 +97,35 @@ exports.ajouter = (req, res, next) => {
     // L'image est passée par Multer et a été enregistrée dans le serveur
     // On recompose l'objet car les genres sont passés en String et on les veut en Array
 
-    // ET VALIDATION
+    // TO DO 
+    // TO DO 
+    // TO DO 
+    // VALIDATION !!!
+    // TO DO 
+    // TO DO 
+    // TO DO 
     var tempFilm = req.body;
-    var film = new Film({
+    var filmTransmis = new Film({
         titre: tempFilm.titre,
         realisateur: tempFilm.realisateur,
         description: tempFilm.description,
         date: tempFilm.date,
         genre: tempFilm.genres.split(','),
-        imageUrl : `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    
-    film.save()
+
+    filmTransmis.save()
         .then(() => res.status(201).json({ message: film.titre + ' correctement ajouté' }))
         .catch(error => res.status(400).json({ error }))
 };
 
-exports.supprimer = (req, res, next) => {
-    film.deleteOne({ _id: req.params.id })
-        .then(() => {
 
-            res.status(200).json({ message: req.params.titre + ' supprimé ' })
-        })
-        .catch(error => res.status(400).json({ error }));
-};
+// cette requête n'est pas encore utilisée
+// exports.supprimer = (req, res, next) => {
+//     film.deleteOne({ _id: req.params.id })
+//         .then(() => {
+
+//             res.status(200).json({ message: req.params.titre + ' supprimé ' })
+//         })
+//         .catch(error => res.status(400).json({ error }));
+// };
--- backend/routes/film.js
+++ backend/routes/film.js
@@ -4,8 +4,12 @@ const filmCtrl = require('../controllers/film');
 const multer = require('../middlewares/multer-config');
 
 router.get('/', filmCtrl.tousLesFilms);
-router.get('/:id', filmCtrl.unFilm);
+router.get('/par_id/:id', filmCtrl.unFilm);
+router.get('/par_genre/:genre', filmCtrl.filmsParGenre);
+router.get('/par_real/:real',filmCtrl.filmsParReal);
+// Route à utiliser si on implémente la recherche par plusieurs genres
+// router.get('/par_genres',filmCtrl.filmsParGenre);
 router.post('/', multer, filmCtrl.ajouter);
-router.delete('/:id',filmCtrl.supprimer);
+// router.delete('/:id',filmCtrl.supprimer);
 
 module.exports = router;
