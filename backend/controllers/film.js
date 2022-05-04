be2801e19dcee21bc78e7e04ecbf3bd5ee45f788
ClementRoelens <clement.roelens@hotmail.com>
2022-05-04 18:53:52 +0200

    Ajout de la randomisation de 25 films transmis par le serveur, plutôt que tous dans le même ordre

--- backend/controllers/film.js
+++ backend/controllers/film.js
@@ -4,12 +4,21 @@ const fs = require('fs');
 exports.tousLesFilms = (req, res, next) => {
     Film.find()
         .then(films => {
-            var filmsTransmis = [];
+            let filmsAtransmettre = [];
+            // Le format de date n'étant pas désiré, on convertit tous les films en JSON pour pouvoir convertir la propriété au format voulu
             films.forEach(film => {
-                var tempFilm = film.toJSON();
+                const tempFilm = film.toJSON();
                 tempFilm.date = tempFilm.date.toLocaleDateString();
-                filmsTransmis.push(tempFilm);
+                filmsAtransmettre.push(tempFilm);
             });
+
+            const filmsTransmis = [];
+            // On va créer une nouvelle Array de 25 films pris au hasard
+            for (i = 0; i < 20 && i < filmsAtransmettre.length; i++) {
+                const rand = Math.round(Math.random() * (filmsAtransmettre.length - 1));
+                filmsTransmis.push(filmsAtransmettre[rand]);
+                filmsAtransmettre = filmsAtransmettre.slice(0, rand).concat(filmsAtransmettre.slice(rand + 1));
+            }
 
             res.status(200).json(filmsTransmis)
         })
@@ -21,37 +30,45 @@ exports.tousLesFilms = (req, res, next) => {
 
 exports.filmsParGenre = (req, res, next) => {
     // On va récupérer tous les films appartenant au moins à un des genres passés en URL
-    var genreAchercher = req.params.genre;
+    const genreAchercher = req.params.genre;
     Film.find()
         .then(films => {
-            var filmsTransmis = [];
+            let filmsAtransmettre = [];
             films.forEach(filmAtrier => {
-                var genresAtrier = filmAtrier.genre;
+                const genresAtrier = filmAtrier.genre;
                 // Le test est true si au moins un des genres du film itéré est le genre cherché
-                if (genresAtrier.includes(genreAchercher)){
+                if (genresAtrier.includes(genreAchercher)) {
                     // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
-                    var tempFilm = filmAtrier.toJSON();
+                    const tempFilm = filmAtrier.toJSON();
                     tempFilm.date = tempFilm.date.toLocaleDateString();
-                    filmsTransmis.push(tempFilm);
+                    filmsAtransmettre.push(tempFilm);
                 }
             });
+
+            const filmsTransmis = [];
+            // On va créer une nouvelle Array de 25 films pris au hasard
+            for (i = 0 , limite = filmsAtransmettre.length; (i < 20) && (i < limite); i++) {
+                const rand = Math.round(Math.random() * (filmsAtransmettre.length - 1));
+                filmsTransmis.push(filmsAtransmettre[rand]);
+                filmsAtransmettre = filmsAtransmettre.slice(0, rand).concat(filmsAtransmettre.slice(rand + 1));
+            }
             res.status(200).json(filmsTransmis);
         })
         .catch(error => res.status(404).json(error))
-    
+
 };
 
 // Si on veut rechercher plusieurs genres 
 // exports.filmsParGenre = (req, res, next) => {
 //     // On va récupérer tous les films appartenant au moins à un des genres passés en URL
-//     var genresDeTri = req.query.genre;
+//     const genresDeTri = req.query.genre;
 //     Film.find()
 //         .then(films => {
-//             var filmsTransmis = [];
+//             const filmsTransmis = [];
 //             films.forEach(filmAtrier => {
-//                 var genresAtrier = filmAtrier.genre;
+//                 const genresAtrier = filmAtrier.genre;
 //                 // Le test est true si au moins un des genres du film itéré est contenu dans l'array des genres cherchés
-//                 var test = genresAtrier.some(genre => genresDeTri.includes(genre));
+//                 const test = genresAtrier.some(genre => genresDeTri.includes(genre));
 //                 // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
 //                 if (test){
 //                     filmsTransmis.push(filmAtrier);
@@ -60,25 +77,32 @@ exports.filmsParGenre = (req, res, next) => {
 //             res.status(200).json(filmsTransmis);
 //         })
 //         .catch()
-    
+
 // };
 
-exports.filmsParReal = (req,res,next) => {
-    var real = req.params.real;
+exports.filmsParReal = (req, res, next) => {
+    const real = req.params.real;
     Film.find()
-    .then(films=>{
-        var filmsTransmis = [];
-        films.forEach(filmAtrier => {
-            if (filmAtrier.realisateur === real){
-                  // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
-                  var tempFilm = filmAtrier.toJSON();
-                  tempFilm.date = tempFilm.date.toLocaleDateString();
-                  filmsTransmis.push(tempFilm);
+        .then(films => {
+            let filmsAtransmettre = [];
+            films.forEach(filmAtrier => {
+                if (filmAtrier.realisateur === real) {
+                    // Si le test est bon, on ajoute ce film à la liste des films qu'on va retourner
+                    const tempFilm = filmAtrier.toJSON();
+                    tempFilm.date = tempFilm.date.toLocaleDateString();
+                    filmsAtransmettre.push(tempFilm);
+                }
+            });
+            const filmsTransmis = [];
+            // On va créer une nouvelle Array de 25 films pris au hasard
+            for (i = 0 , limite = filmsAtransmettre.length; (i < 20) && (i < limite); i++) {
+                const rand = Math.round(Math.random() * (filmsAtransmettre.length - 1));
+                filmsTransmis.push(filmsAtransmettre[rand]);
+                filmsAtransmettre = filmsAtransmettre.slice(0, rand).concat(filmsAtransmettre.slice(rand + 1));
             }
-        });
-        res.status(200).json(filmsTransmis);
-    })
-    .catch(error => res.status(404).json(error))
+            res.status(200).json(filmsTransmis);
+        })
+        .catch(error => res.status(404).json(error))
 };
 
 exports.unFilm = (req, res, next) => {
@@ -86,7 +110,7 @@ exports.unFilm = (req, res, next) => {
         _id: req.params.id
     }).then(filmBrut => {
         // Le format de date n'étant pas désiré, on convertit l'objet en JSON pour pouvoir convertir la propriété au format voulu
-        var filmTransmis = filmBrut.toJSON();
+        const filmTransmis = filmBrut.toJSON();
         filmTransmis.date = filmTransmis.date.toLocaleDateString();
         res.status(200).json(filmTransmis);
     })
@@ -104,8 +128,8 @@ exports.ajouter = (req, res, next) => {
     // TO DO 
     // TO DO 
     // TO DO 
-    var tempFilm = req.body;
-    var filmTransmis = new Film({
+    const tempFilm = req.body;
+    const filmTransmis = new Film({
         titre: tempFilm.titre,
         realisateur: tempFilm.realisateur,
         description: tempFilm.description,
